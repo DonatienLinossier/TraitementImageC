55cbac9977cf7d45e42bc1ba38753da9742eeceb
DonatienLinossier <donatien.linossier@gmail.com>
2023-04-07 16:18:52 +0200

    07/04/2023 debut d'un programme plus propre

--- main.c
+++ main.c
@@ -4,42 +4,88 @@
 #include <sys/types.h>
 
 
-typedef struct {
-    char* type[2];
-    int* size[4];
-    int* createdBy[2];
-    int* createdByBis[2];
-    int* offset[4];  
-} header;
-
-
 int decodageLittleEndian(int bytes[], int SIZE) {
     int resulttotal = 0;
     for (int i =0; i<SIZE; i++) {
         int nb = bytes[i];
         int result = 0;
         int decomposition[8];
-        //printf("\nDécomposition de %d = ",  bytes[i]);
         for(int h=0; h<8; h++) {  
                 decomposition[h] = nb%2;  
-                //printf("%d", decomposition[h]);
                 result += decomposition[h] * pow(2, h) * pow(2, 8*i);
                 nb = nb/2;
         }
-        //printf(" - %d devient : %d;", bytes[i], result);
         resulttotal+= result;
-        //printf("\n %d * 2**8*%d, %d \n", bytes[i], i, bytes[i] * pow(2, 8*i));
         
     }
-    //printf("\nResult final : %d\n", resulttotal);
     return resulttotal;
 }
 
 
+
+typedef struct {
+    char type[2];
+    int size;
+    int createdBy;
+    int createdByBis;
+    int offset;  
+} Header;
+
+void affichageHeader(Header header) {
+    printf("Information sur le header : \n");
+    printf("    type : %s\n", header.type);
+    printf("    Image size : %d\n", header.size);
+    printf("    createdBy : %d\n", header.createdBy);
+    printf("    createdByBis : %d\n", header.createdByBis);
+    printf("    Offset : %d\n", header.offset);
+}
+
+typedef struct {
+    int sizeHeader;
+    int width;
+    int height;
+    int nbColorplane; //must be 1
+    int nbBitByPixel;
+    int compressionMethod;
+    int imageSize;
+    int horizontalResolution;
+    int verticalResolution;
+    int nbColorInPalette;
+    int nbOfImportantColorUsed;
+} DibHeader;
+
+void affichageDIBHeader(DibHeader dibHeader) {
+    printf("Information sur le header : \n");
+    printf("    sizeHeader : %d\n", dibHeader.sizeHeader);
+    printf("    Image width : %d\n", dibHeader.width);
+    printf("    Image height : %d\n", dibHeader.height);
+    printf("    nbColorplane : %d\n", dibHeader.nbColorplane);
+    printf("    nbBitByPixel : %d\n", dibHeader.nbBitByPixel);
+    printf("    compressionMethod : %d\n", dibHeader.compressionMethod);
+    printf("    imageSize : %d\n", dibHeader.imageSize);
+    printf("    horizontalResolution : %d\n", dibHeader.horizontalResolution);
+    printf("    verticalResolution : %d\n", dibHeader.verticalResolution);
+    printf("    nbColorInPalette : %d\n", dibHeader.nbColorInPalette);
+    printf("    nbOfImportantColorUsed : %d\n", dibHeader.nbOfImportantColorUsed);
+}
+
+typedef struct {
+    Header header;
+    DibHeader dibHeader;
+    int** image;
+} Image;
+
+
+
+
+
 int main()
 {
-    int WIDTH;
-    int HEIGHT;
+
+    Image image;
+    image.dibHeader.height = 0;
+    image.dibHeader.width = 0;
+
     int caractereActuel = 0;
     FILE* fichier = NULL;
 
@@ -58,11 +104,31 @@ int main()
             printf("%d ", caractereActuel);
             tab[j] = caractereActuel;
         }
+        switch(i) {
+            case 0: 
+                image.header.type[0] = tab[0];
+                image.header.type[1] = tab[1];
+                break;
+            case 1:
+                image.header.size = decodageLittleEndian(tab, SizeElementsHeaders[i]);
+                break;
+            case 2:
+                image.header.createdBy = decodageLittleEndian(tab, SizeElementsHeaders[i]);
+                break;
+            case 3: 
+                image.header.createdByBis = decodageLittleEndian(tab, SizeElementsHeaders[i]);
+                break;
+            case 4:
+                image.header.offset = decodageLittleEndian(tab, SizeElementsHeaders[i]);
+                break;
+        }
         elementsHeaders[i] = tab;
+
+        
         
         printf("\n");
     }
-
+    affichageHeader(image.header);
 
     int SizeElementsDIBHeaders[11] = {4, 4, 4, 2, 2, 4, 4, 4, 4, 4, 4};
     int* elementsDIBHeaders[11];   
@@ -76,38 +142,74 @@ int main()
             tab[j] = caractereActuel;
         }
         elementsDIBHeaders[i] = tab;
-        if(i == 1) {
-            WIDTH = decodageLittleEndian(tab, 4);
+        /*if(i == 1) {
+            image.dibHeader.width = decodageLittleEndian(tab, 4);
         } else if (i == 2) {
-            HEIGHT = decodageLittleEndian(tab, 4);
+            image.dibHeader.height = decodageLittleEndian(tab, 4);
+        }*/
+        switch(i) {
+            case 0: 
+                image.dibHeader.sizeHeader = decodageLittleEndian(tab, SizeElementsHeaders[i]);
+                break;
+            case 1:
+                image.dibHeader.width = decodageLittleEndian(tab, SizeElementsHeaders[i]);
+                break;
+            case 2:
+                image.dibHeader.height = decodageLittleEndian(tab, SizeElementsHeaders[i]);
+                break;
+            case 3: 
+                image.dibHeader.nbColorplane = decodageLittleEndian(tab, SizeElementsHeaders[i]);
+                break;
+            case 4:
+                image.dibHeader.nbBitByPixel = decodageLittleEndian(tab, SizeElementsHeaders[i]);
+                break;
+            case 5:
+                image.dibHeader.compressionMethod = decodageLittleEndian(tab, SizeElementsHeaders[i]);
+                break;
+            case 6:
+                image.dibHeader.imageSize = decodageLittleEndian(tab, SizeElementsHeaders[i]);
+                break;
+            case 7:
+                image.dibHeader.horizontalResolution = decodageLittleEndian(tab, SizeElementsHeaders[i]);
+                break;
+            case 8:
+                image.dibHeader.verticalResolution = decodageLittleEndian(tab, SizeElementsHeaders[i]);
+                break;
+            case 9:
+                image.dibHeader.nbColorInPalette = decodageLittleEndian(tab, SizeElementsHeaders[i]);
+                break;
+            case 10:
+                image.dibHeader.nbOfImportantColorUsed = decodageLittleEndian(tab, SizeElementsHeaders[i]);
+                break;
         }
         
         printf("\n");
     }
-
-    printf("WIDTH : %d; HEIGHT : %d", WIDTH, HEIGHT);
+    printf("preaff");
+    affichageDIBHeader(image.dibHeader);
+    printf("WIDTH : %d; HEIGHT : %d", image.dibHeader.width, image.dibHeader.height);
 
 
     printf("\nDecodage IMG...");
-    int image[HEIGHT][WIDTH][3];
+    int imagee[image.dibHeader.height][image.dibHeader.width][3];
     int i = 0;
 
 
     //vérif le décalage, peut etre un pb. Peut etre qu'on ne commence pas au db; pb avec le -1??
-    while(caractereActuel != EOF && i!=3*600*800) {
+    while(caractereActuel != EOF && i!=3*image.dibHeader.width*image.dibHeader.height) {
         caractereActuel = fgetc(fichier);
         int indiceRGB = i%3;
-        int indiceWIDTH = (i/3)%WIDTH;
-        int indiceHEIGHT = (i/3)/WIDTH;
+        int indiceWIDTH = (i/3)%image.dibHeader.height;
+        int indiceHEIGHT = (i/3)/image.dibHeader.height;
         //printf("%d: [%d][%d][%d] ", i, indiceHEIGHT, indiceWIDTH, indiceRGB);
-        image[indiceHEIGHT][indiceWIDTH][indiceRGB] = caractereActuel;
+        imagee[indiceHEIGHT][indiceWIDTH][indiceRGB] = caractereActuel;
         i++;
     }
 
     printf("Done\n");
     printf("Affichage Finale :\n");
-    for(int i = 0; i<WIDTH; i++) {
-        printf("[120][%d] : (%d, %d, %d) \n", i, image[120][i][0], image[120][i][1], image[120][i][2]);
+    for(int i = 0; i<image.dibHeader.height; i++) {
+        //printf("[120][%d] : (%d, %d, %d) \n", i, imagee[120][i][0], imagee[120][i][1], imagee[120][i][2]);
     }
     return 0;
     
