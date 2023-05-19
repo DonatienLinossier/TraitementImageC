3f163d38bdccba803629981667121fd7c76b5b57
DonatienLinossier <donatien.linossier@gmail.com>
2023-05-19 11:09:08 +0200

    creation fichier main.c. creation fichier interface.h interface.c - Suppresion du main (maintenant dans main.c)

---
+++ interface.h
@@ -0,0 +1,26 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+#include <stdio.h>
+#include "gestionFichierImg.h"
+
+FILE* fileChoice();
+void resizeInterface();
+void rognerInterface();
+void affichageASCIIInterface(Image* image);
+void BlackAndWhiteInterface();
+void rotationInterface();
+void luminositeInterface();
+void contrasteInterface();
+void flouInterface();
+void BinariserInterface();
+void inverserCouleursInterface();
+void symetrieInterface();
+void steganographieInterface(Image* image);
+void saveImageInterface(Image* image);
+void changeImageInterface(FILE* activeFile, Image* img);
+int choiceImageManipulation();
+
+
+
+#endif
