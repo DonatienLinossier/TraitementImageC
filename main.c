0fe911ed81662153b156dca1c9290453a34fabdf
Asma <asmakajeiou@gmail.com>
2023-05-27 18:52:02 +0200

    rotate

--- main.c
+++ main.c
@@ -49,7 +49,7 @@ int main() {
                 grayscaleInterface(&img,selection);
                 break;
             case 6:
-                rotateInterface(&img);
+                rotateInterface(&img, selection);
                 break;
             case 7:
                 brightnessInterface(&img,selection);
